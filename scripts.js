e193ff244d80179989703ac1450136855505af2e
maletros <104573851+maletros@users.noreply.github.com>
2024-02-26 15:14:17 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', () => {
-  //opções de cartas
+  // Opções de cartas com seus nomes e imagens
   const cards = [
     {
       name: 'coelho',
@@ -49,73 +49,110 @@ document.addEventListener('DOMContentLoaded', () => {
       name: 'urso',
       img: 'images/urso.jpg'
     },
-  ]
+  ];
 
-  //embaralhar todas as cartas
-  cards.sort(() => 0.5 - Math.random())
+  // Embaralhar todas as cartas
+  cards.sort(() => 0.5 - Math.random());
 
-  //recupaerar elementos
-  const board = document.querySelector('.board')
-  const resultView = document.querySelector('#result')
-  let cardsChosen = [] //cartas escolhidas
-  let cardsChosenId = [] //ids das cartas escolhidas para caso de click na mesma imagem
-  let cardsWon = [] //cartas combinadas
+  // Recuperar elementos do DOM
+  const board = document.querySelector('.board'); // o quadro de jogo
+  const resultView = document.querySelector('#result'); // a visão do resultado
+  const playButton = document.getElementById('playButton'); // o botão "Jogar!"
+  const reloadButton = document.getElementById('reloadButton'); // o botão "Jogar Novamente !"
+  let cardsChosen = []; // Armazena as cartas escolhidas
+  let cardsChosenId = []; // Armazena os IDs das cartas escolhidas para identificação
+  let cardsWon = []; // Armazena as cartas combinadas
 
-  //criar o quadro de cartas
+  // Criar o quadro de cartas
   function createBoard() {
     for (let i = 0; i < cards.length; i++) {
-      const card = document.createElement('img')
-      card.setAttribute('src', 'images/interroga.jpg')
-      card.setAttribute('data-id', i)
-      card.addEventListener('click', flipCard)
-      board.appendChild(card)
+      const card = document.createElement('img');
+      card.setAttribute('src', 'images/interroga.jpg'); // Todas as cartas começam com a face para baixo
+      card.setAttribute('data-id', i); // Definir o ID da carta
+      card.addEventListener('click', flipCard); // Adicionar evento de clique para virar a carta
+      board.appendChild(card); // Adicionar a carta ao quadro de jogo
     }
   }
 
-  //checagem de combinações
+  // Checar se as cartas escolhidas formam um par
   function checkForMatch() {
-    const cards = document.querySelectorAll('img')
-    const optionOneId = cardsChosenId[0]
-    const optionTwoId = cardsChosenId[1]
+    const cards = document.querySelectorAll('img'); // Selecionar todas as cartas
+    const optionOneId = cardsChosenId[0]; // ID da primeira carta escolhida
+    const optionTwoId = cardsChosenId[1]; // ID da segunda carta escolhida
     
-    //verificar clique na mesma imagem 
-    if(optionOneId == optionTwoId) {
-      cards[optionOneId].setAttribute('src', 'images/interroga.jpg')
-      cards[optionTwoId].setAttribute('src', 'images/interroga.jpg')
-      alert('Você clicou na mesma imagem')
+    // Verificar se o mesmo cartão foi clicado duas vezes
+    if(optionOneId === optionTwoId) {
+      // Reverter as cartas selecionadas de volta para a face para baixo
+      cards[optionOneId].setAttribute('src', 'images/interroga.jpg');
+      cards[optionTwoId].setAttribute('src', 'images/interroga.jpg');
+      alert('Você clicou na mesma imagem');
     }
-    //verificar combinação se click em imagens diferentes
+    // Verificar se as imagens das duas cartas selecionadas são iguais
     else if (cardsChosen[0] === cardsChosen[1]) {
-      alert('Você encontrou uma combinação')
-      cards[optionOneId].setAttribute('src', 'images/check.png')
-      cards[optionTwoId].setAttribute('src', 'images/check.png')
-      cards[optionOneId].removeEventListener('click', flipCard)
-      cards[optionTwoId].removeEventListener('click', flipCard)
-      cardsWon.push(cardsChosen)
+      // Se as imagens forem iguais, indicar uma combinação
+      alert('Você encontrou uma combinação');
+      // Alterar a imagem das cartas para mostrar a combinação
+      cards[optionOneId].setAttribute('src', 'images/check.png');
+      cards[optionTwoId].setAttribute('src', 'images/check.png');
+      // Remover o evento de clique das cartas combinadas
+      cards[optionOneId].removeEventListener('click', flipCard);
+      cards[optionTwoId].removeEventListener('click', flipCard);
+      // Adicionar o par de cartas combinadas à lista de cartas ganhas
+      cardsWon.push(cardsChosen);
     } else {
-      cards[optionOneId].setAttribute('src', 'images/interroga.jpg')
-      cards[optionTwoId].setAttribute('src', 'images/interroga.jpg')
-      alert('Errou, tente novamente')
+      // Se as imagens forem diferentes, reverter as cartas de volta para a face para baixo
+      cards[optionOneId].setAttribute('src', 'images/interroga.jpg');
+      cards[optionTwoId].setAttribute('src', 'images/interroga.jpg');
+      alert('Errou, tente novamente');
     }
-    cardsChosen = []
-    cardsChosenId = []
-    //mostrar placar
-    resultView.textContent = 'Pares Encontrados: '+cardsWon.length
-    if  (cardsWon.length === cards.length/2) {
-      resultView.textContent = 'Parabéns! Você conseguiu encontrar todas as cartas'
+    // Limpar as listas de cartas escolhidas e IDs
+    cardsChosen = [];
+    cardsChosenId = [];
+    // Atualizar o placar mostrando quantos pares foram encontrados
+    resultView.textContent = 'Pares Encontrados: ' + cardsWon.length;
+    // Verificar se todas as cartas foram combinadas
+    if  (cardsWon.length === cards.length / 2) {
+      // Se todas as cartas foram combinadas, exibir uma mensagem de parabéns
+      resultView.textContent = 'Parabéns! Você conseguiu encontrar todas as cartas';
+      reloadButton.style.display = 'block'; // Exibir o botão "Jogar Novamente !" quando o jogo é concluído
     }
   }
 
-  //virar as cartas
+  // Virar as cartas
   function flipCard() {
-    let cardId = this.getAttribute('data-id')
-    cardsChosen.push(cards[cardId].name)
-    cardsChosenId.push(cardId)
-    this.setAttribute('src', cards[cardId].img)
-    if (cardsChosen.length ===2) {
-      setTimeout(checkForMatch, 500)
+    let cardId = this.getAttribute('data-id'); // Obter o ID da carta clicada
+    cardsChosen.push(cards[cardId].name); // Adicionar o nome da carta às cartas escolhidas
+    cardsChosenId.push(cardId); // Adicionar o ID da carta às IDs das cartas escolhidas
+    this.setAttribute('src', cards[cardId].img); // Mudar a imagem da carta para mostrar seu conteúdo
+    if (cardsChosen.length === 2) {
+      // Se duas cartas foram escolhidas, verificar se elas formam um par depois de um breve intervalo
+      setTimeout(() => {
+        checkForMatch();
+      }, 500);
     }
   }
 
-  createBoard()
-})
+  // Adicionar evento de clique ao botão "Jogar!"
+  playButton.addEventListener('click', () => {
+    playButton.style.display = 'none'; // Esconder o botão após ser clicado
+
+    // Mostrar imagens por 1 segundo ao clicar no botão "Jogar!"
+    document.querySelectorAll('img').forEach(card => {
+      card.setAttribute('src', cards[card.getAttribute('data-id')].img);
+    });
+    setTimeout(() => {
+      // Esconder as cartas após 1 segundo
+      document.querySelectorAll('img').forEach(card => {
+        card.setAttribute('src', 'images/interroga.jpg');
+      });
+    }, 1000);
+  });
+
+  // Adicionar evento de clique ao botão "Jogar Novamente!"
+  reloadButton.addEventListener('click', () => {
+    location.reload(); // Recarregar a página para reiniciar o jogo
+  });
+
+  // Iniciar o jogo criando o quadro de cartas
+  createBoard();
+});
